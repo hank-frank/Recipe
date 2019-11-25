e536971256a238a6bb27ef952b5c8751497c4576
hankfrank <henryfrank1@yahoo.com>
2019-11-24 20:49:20 -0800

    searching by ingredient added, console.logging results

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -6,14 +6,17 @@ import importedRecipe from "./recipes/recipes.json"
 
 import Header from "./header.jsx"
 import Main from "./Main.jsx"
+import Search from "./search.jsx"
 // import Chart from "./chart.jsx"
 
 function App () {
   const [chickenRecipe, setChickenRecipe] = useState([]);
-  const[cocktail, setCocktail] = useState({});
+  const [cocktail, setCocktail] = useState({});
+  const [searchResult, setSearchResult] = useState([]);
 
 
   function apiTest () {
+    console.log(searchResult)
     fetch(`/cocktails`)
     .then((response) => {
       return response = response.json()
@@ -24,10 +27,26 @@ function App () {
     .catch(err => console.log(`whoopsies`, err))
   };
 
+  const test2 = (searchValue) => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchValue}`)
+    .then((response) => {
+      return response = response.json()
+    })
+    .then ((data) => {
+      setSearchResult(data.drinks)
+      
+    })
+  }
+
   return (
     <>
     <Header />
     <button className="test-button" onClick={ ()=> apiTest()}>Testing!</button>
+    <button className="test-button" onClick={ ()=> test2()}>Test 2</button>
+    <Search 
+    search = { test2 }
+    resultArray = { searchResult }
+    />
     <Main 
       importedRecipe = { importedRecipe }
     />      
